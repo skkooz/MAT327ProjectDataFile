74b965c0b5ba1659becc40d8ac80b32ae6d13b02
skkooz <49208158+skkooz@users.noreply.github.com>
2023-11-07 11:43:21 -0500

    Add files via upload

---
+++ Milestone5.R
@@ -0,0 +1,23 @@
+# Using mean, median, var and sd along with actual_mean_temp:
+mean(mydata$actual_mean_temp)
+median(mydata$actual_mean_temp)
+var(mydata$actual_mean_temp)
+sd(mydata$actual_mean_temp)
+
+# Using mean, median, var and sd along with actual_min_temp:
+mean(mydata$actual_min_temp)
+median(mydata$actual_min_temp)
+var(mydata$actual_min_temp)
+sd(mydata$actual_min_temp)
+
+# Using mean, median, var and sd along with actual_max_temp:
+mean(mydata$actual_max_temp)
+median(mydata$actual_max_temp)
+var(mydata$actual_max_temp)
+sd(mydata$actual_max_temp)
+
+# Using mean, median, var and sd along with average_min_temp:
+mean(mydata$average_min_temp)
+median(mydata$average_min_temp)
+var(mydata$average_min_temp)
+sd(mydata$average_min_temp)
