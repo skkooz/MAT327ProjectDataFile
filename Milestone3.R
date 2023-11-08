0ca067170f9e26731826838e2fbcd0e969a25209
skkooz <49208158+skkooz@users.noreply.github.com>
2023-11-07 19:12:31 -0500

    Add files via upload

---
+++ Milestone3.R
@@ -0,0 +1,15 @@
+# 1st Column
+data <- KCLT
+hist(KCLT$average_max_temp)
+
+# 2nd Column
+data <- KCLT
+hist(KCLT$actual_mean_temp)
+
+# 3rd Column
+data <- KCLT
+hist(KCLT$actual_min_temp)
+
+# 4th Column
+data <- KCLT
+hist(KCLT$average_precipitation)
