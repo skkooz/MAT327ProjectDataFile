31e15f53e12b16f168afb2c5eb45cb908d5f1925
skkooz <49208158+skkooz@users.noreply.github.com>
2023-11-30 19:25:22 -0500

    Add files via upload
    I used the KCLT portion of my dataset. KCLT can be found in this repository to be downloaded in order to see the results of my code, up and running. Otherwise, I have uploaded a screenshot of my results on my MAT 327 Project page.

---
+++ M7ConfidenceIntervals.R
@@ -0,0 +1,8 @@
+# Code that gives the 95% confidence interval along with the mean of x
+t.test(KCLT$actual_mean_temp) 
+# Calculates the margin of error
+62.70258 - 61.04932 
+#Code that gives the 95% confidence interval along with the mean of x
+t.test(KCLT$record_min_temp)
+# Calculates the margin of error
+33.36285 - 31.46575
